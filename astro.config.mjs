d11eea21da4b9c4dfb278838c8a473983c5705ef
Saulo <saulo@tokilabs.io>
2024-03-28 03:46:23 +0000

    Fix open graph image

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon"
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://svallory.github.io',
+  site: 'https://saulo.engineer',
   compressHTML: true,
   integrations: [mdx(), icon(), tailwind({
     applyBaseStyles: false,
